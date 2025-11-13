246da4bf8541b906003d8e71a21627d61a6af0aa
Xoz99 <andrianadiwahyono2005@gmail.com>
2025-11-13 16:06:46 +0700

    sasamo

--- google-apps-script.js
+++ google-apps-script.js
@@ -16,6 +16,7 @@
 
 var sheetName = 'QRIS' // Default sheet
 var SPREADSHEET_ID = '1vo-s2biXePgPsMdbwzhI8cmyd1AihH_Embz8kRWYtzw' // Hardcode Spreadsheet ID
+var PARENT_FOLDER_ID = '1VvoTKpCD8kiLID413cchQc7ljkpiDnAWzmaz1-duHDbvD3XVcAoyqALzkxYcbDoYC_s3mX0z' // Parent folder di Google Drive
 
 function intialSetup() {
   var activeSpreadsheet = SpreadsheetApp.getActiveSpreadsheet()
@@ -69,12 +70,31 @@ function doPost(e) {
     Logger.log('Headers: ' + JSON.stringify(headers))
     Logger.log('Next row: ' + nextRow)
 
+    // Save image ke Drive jika ada
+    var imageLink = ''
+    if (data.imageBase64 && data.imageBase64.length > 100) { // Minimal panjang base64
+      try {
+        Logger.log('Processing image, size: ' + data.imageBase64.length)
+        imageLink = saveImageToDrive(data.imageBase64, data.nama, paymentMethod)
+        Logger.log('Image saved: ' + imageLink)
+      } catch(imgError) {
+        Logger.log('Image save error: ' + imgError.toString())
+        imageLink = 'Error: ' + imgError.toString()
+      }
+    } else {
+      Logger.log('No valid image data received')
+      imageLink = 'No image'
+    }
+
     // Map data ke kolom berdasarkan header
     var newRow = headers.map(function(header) {
       if (header === 'timestamp') {
         return new Date()
       } else if (header === 'status') {
         return 'Pending'
+      } else if (header === 'imageBase64' || header === 'bukti_pembayaran' || header === 'bukti' || header === 'image') {
+        // Ganti imageBase64 dengan link
+        return imageLink
       } else if (header === 'items') {
         // Format items jika ada
         try {
@@ -124,5 +144,86 @@ function doPost(e) {
   }
   finally {
     lock.releaseLock()
+  }
+}
+
+/**
+ * Save image dari base64 ke Google Drive
+ */
+function saveImageToDrive(base64String, customerName, paymentMethod) {
+  try {
+    if (!base64String || base64String.length < 100) {
+      throw new Error('Invalid base64 string')
+    }
+
+    Logger.log('Starting image save...')
+    Logger.log('Customer: ' + customerName)
+    Logger.log('Payment Method: ' + paymentMethod)
+
+    // Get parent folder
+    var parentFolder = DriveApp.getFolderById(PARENT_FOLDER_ID)
+    Logger.log('Parent folder: ' + parentFolder.getName())
+
+    // Create atau get folder "SIP DEH - Bukti Pembayaran" di dalam parent folder
+    var folderName = 'SIP DEH - Bukti Pembayaran'
+    var existingFolders = parentFolder.getFoldersByName(folderName)
+    var folder
+    if (existingFolders.hasNext()) {
+      folder = existingFolders.next()
+      Logger.log('Folder exists in parent')
+    } else {
+      folder = parentFolder.createFolder(folderName)
+      Logger.log('Folder created in parent')
+    }
+
+    // Create atau get subfolder per payment method
+    var subFolderName = paymentMethod === 'bank' ? 'Bank Transfer' : 'QRIS'
+    var subFolders = folder.getFoldersByName(subFolderName)
+    var subFolder
+    if (subFolders.hasNext()) {
+      subFolder = subFolders.next()
+      Logger.log('SubFolder exists: ' + subFolderName)
+    } else {
+      subFolder = folder.createFolder(subFolderName)
+      Logger.log('SubFolder created: ' + subFolderName)
+    }
+
+    // Decode base64
+    var base64Data = base64String
+    if (base64String.indexOf(',') > 0) {
+      base64Data = base64String.split(',')[1]
+    }
+
+    Logger.log('Base64 data length: ' + base64Data.length)
+
+    var decodedData = Utilities.base64Decode(base64Data)
+    var blob = Utilities.newBlob(decodedData, 'image/jpeg')
+
+    Logger.log('Blob created, size: ' + blob.getBytes().length)
+
+    // Create filename
+    var timestamp = new Date().getTime()
+    var sanitizedName = customerName.replace(/[^a-zA-Z0-9]/g, '_')
+    var filename = sanitizedName + '_' + timestamp + '.jpg'
+
+    Logger.log('Filename: ' + filename)
+
+    // Save to folder
+    var file = subFolder.createFile(blob)
+    file.setName(filename)
+
+    Logger.log('File created: ' + file.getId())
+
+    // Make file shareable dan get link
+    file.setSharing(DriveApp.Access.ANYONE, DriveApp.Permission.VIEW)
+    var fileUrl = file.getUrl()
+
+    Logger.log('Image file created successfully: ' + fileUrl)
+    return fileUrl
+
+  } catch (error) {
+    Logger.log('Error saving image: ' + error.toString())
+    Logger.log('Stack: ' + error.stack)
+    throw error
   }
 }
